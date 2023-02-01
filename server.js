6f2d61141e48f03610644c2674583f29252b30ea
Antonio <89131333+Antomico205@users.noreply.github.com>
2023-02-01 13:49:31 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require ('express')
 const app = express();
 
+const users = [
+    {id: 1, name: "Peter Burkard"},
+    {id: 2, name: "Janin Möckel"},
+    {id: 3, name: "Nicola Süss"},
+    {id: 4, name: "Leano Hunzicker"},
+    {id: 5, name: "Mila Jud"},
+    {id: 5, name: "Mia Mutard"},
+    
+];
+
 app.get('/',(req,res) => {
     res.send("Hello World")
 });
